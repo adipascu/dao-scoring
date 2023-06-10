8bedbab8359faa71345560ec36c4541bd818cc11
Adrian Pascu <mail@adipascu.ro>
2023-06-10 09:49:39 +0200

    Add placeholder questions

--- src/app/survey/page.tsx
+++ src/app/survey/page.tsx
@@ -1,4 +1,41 @@
+"use client";
+import React, { useState } from "react";
+
+const QUESTIONS = ["Question 1?", "Question 2?", "Question 3?"];
+const cardStyle = {} as const;
 const Survey = () => {
-  return <span>Survey goes here</span>;
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [answers, setAnswers] = useState<string[]>([]);
+
+  const handleAnswer = (answer: string) => {
+    setAnswers((prev: string[]) => [...prev, answer]);
+    setCurrentQuestionIndex((prev) => prev + 1);
+  };
+
+  return (
+    <>
+      {currentQuestionIndex < QUESTIONS.length ? (
+        <div style={cardStyle}>
+          <h2>{QUESTIONS[currentQuestionIndex]}</h2>
+          <button onClick={() => handleAnswer("Yes")}>Yes</button>
+          <button onClick={() => handleAnswer("No")}>No</button>
+        </div>
+      ) : (
+        <div>
+          <h2>
+            Thanks for completing the questionnaire. Here are your answers:
+          </h2>
+          <ul>
+            {answers.map((answer, index) => (
+              <li key={index}>
+                {QUESTIONS[index]}: {answer}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </>
+  );
 };
+
 export default Survey;
