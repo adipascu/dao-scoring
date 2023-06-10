0e4cf20078f3ae2a692efb782d07ebe922e90b57
Adrian Pascu <mail@adipascu.ro>
2023-06-10 09:03:41 +0200

    Add result page

---
+++ src/app/result/page.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Result() {
+  return (
+    <>
+      <Head>
+        <title>Result page</title>
+      </Head>
+      <span>Hello world</span>;
+    </>
+  );
+}
