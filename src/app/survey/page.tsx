beec82c564a12fb3e3dba0e2f7f28bf2cbd29aa3
Adrian Pascu <mail@adipascu.ro>
2023-06-11 03:48:29 +0200

    Add score calculation

--- src/app/survey/page.tsx
+++ src/app/survey/page.tsx
@@ -7,8 +7,8 @@ import {
   Text,
   Button,
 } from "@chakra-ui/react";
-import React, { useCallback, useState } from "react";
-import QUESTIONS, { Question } from "../questions";
+import React, { useCallback, useMemo, useState } from "react";
+import QUESTIONS, { Option, Question } from "../questions";
 const cardStyle = {
   display: "flex",
   flexDirection: "column",
@@ -27,33 +27,42 @@ const Card = ({
   onAnswer,
 }: {
   question: Question;
-  onAnswer: (answer: string) => void;
+  onAnswer: (answer: Option) => void;
 }) => {
-  const answerYes = useCallback(() => onAnswer("Yes"), [onAnswer]);
-  const answerNo = useCallback(() => onAnswer("No"), [onAnswer]);
   return (
     <ChakraCard style={cardStyle}>
       <CardBody>
         <Text>{question.text}</Text>
-        {question.options.map((option, index) => (
-          <Button key={option.text} onClick={() => onAnswer(option.text)}>
+        {question.options.map((option) => (
+          <Button key={option.text} onClick={() => onAnswer(option)}>
             {option.text}
           </Button>
         ))}
-        {/* <Button onClick={answerYes}>Yes</Button> */}
-        {/* <Button onClick={answerNo}>No</Button> */}
       </CardBody>
     </ChakraCard>
   );
 };
 const Survey = () => {
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-  const [answers, setAnswers] = useState<string[]>([]);
+  const [answers, setAnswers] = useState<Option[]>([]);
 
-  const handleAnswer = (answer: string) => {
-    setAnswers((prev: string[]) => [...prev, answer]);
+  const handleAnswer = (answer: Option) => {
+    setAnswers((prev: Option[]) => [...prev, answer]);
     setCurrentQuestionIndex((prev) => prev + 1);
   };
+
+  const agregateScore = useMemo(() => {
+    return answers.reduce(
+      (acc, { effect }) => {
+        if (!effect) {
+          return acc;
+        }
+        const { dimension, impact } = effect;
+        return { ...acc, [dimension]: acc[dimension] + impact };
+      },
+      { D: 0, A: 0, O: 0 }
+    );
+  }, [answers]);
 
   return (
     <div style={pageStyle}>
@@ -70,7 +79,15 @@ const Survey = () => {
           <ul>
             {answers.map((answer, index) => (
               <li key={index}>
-                {QUESTIONS[index].text}: {answer}
+                {QUESTIONS[index].text}: {answer.text}
+              </li>
+            ))}
+          </ul>
+          <h2>Here is your aggregate score:</h2>
+          <ul>
+            {Object.entries(agregateScore).map(([dimension, score]) => (
+              <li key={dimension}>
+                {dimension}: {score}
               </li>
             ))}
           </ul>
